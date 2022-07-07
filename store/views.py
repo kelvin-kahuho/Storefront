4a67a0a915cb65575462e1b087b8f8c97851a5df
ke-stack <kingkevo254@gmail.com>
2022-07-07 12:27:07 +0300

    commit

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from venv import create
 from django.shortcuts import render
 from .models import *
 
@@ -9,7 +10,14 @@ def store(request):
   })
 
 def cart(request):
-  context = {}
+  if request.user.is_authenticated:
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    items = order.orderitem.set.all()
+  else:
+    items=[]
+    
+  context = {'items'}
   return render(request, 'store/cart.html', context)
 
 def checkout(request):
