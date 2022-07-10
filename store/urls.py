b025b17e36a81422a3ce8beba6f6950cc1f8d2a0
ke-stack <kingkevo254@gmail.com>
2022-07-10 23:41:10 +0300

    commit

--- store/urls.py
+++ store/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
   path("", views.store, name="store"),
   path("cart/", views.cart, name="cart"),
   path("checkout/", views.checkout, name="checkout"),
+
+  path("update_item/", views.checkout, name="update_item"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
+from argparse import Action
+from itertools import product
 from venv import create
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 from .models import *
 
 # Create your views here.
@@ -31,4 +35,30 @@ def checkout(request):
       order = {'get_cart_total':0, 'get_cart_items':0}
 
   context = {'items':items, 'order':order}
-  return render(request, 'store/checkout.html', context)
+  return render(request, 'store/checkout.html', context)\
+
+def updateItem(request):
+    
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+    
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse('Item was added', safe=False)
