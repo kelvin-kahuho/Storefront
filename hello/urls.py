086eb5b2ff24c3134a525af18e7f51aa299b7198
ke-stack <kingkevo254@gmail.com>
2022-07-28 19:30:57 +0300

    commit

--- hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .templates.hello import views
-
-#URL configuration
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:name>", views.greet, name="greet")
-]
--- newyear/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-#URL configuration
-urlpatterns = [
-    path("", views.index, name="index"),
-]
