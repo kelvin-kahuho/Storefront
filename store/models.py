e354c996ad602f7c118a07d5304868eccaa9e726
ke-stack <kingkevo254@gmail.com>
2022-08-03 22:59:20 +0300

    commit

--- store/models.py
+++ store/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 # Create your models here.
 
 class Customer(models.Model):
---
+++ store/recommendations.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from recommends.providers import RecommendationProvider
+from recommends.providers import recommendation_registry
+
+from .models import Product, ProductRating
+
+class ProductRecommendationProvider(RecommendationProvider):
+    def get_users(self):
+        return User.objects.filter(is_active=True, votes__isnull=False).distinct()
+
+    def get_items(self):
+        return Product.objects.all()
+
+    def get_ratings(self, obj):
+        return ProductRating.objects.filter(product=obj)
+
+    def get_rating_score(self, rating):
+        return rating.score
+
+    def get_rating_site(self, rating):
+        return rating.site
+
+    def get_rating_user(self, rating):
+        return rating.user
+
+    def get_rating_item(self, rating):
+        return rating.product
+
+recommendation_registry.register(ProductRating, [Product], ProductRecommendationProvider)
