ce355b465e096e43c5c966f59eb4a799e2caa130
ke-stack <kingkevo254@gmail.com>
2022-08-11 06:37:28 +0300

    commit

--- storefront/settings.py
+++ storefront/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'store/static')
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 MEDIA_URL = '/images/'
 
